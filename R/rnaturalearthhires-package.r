b32afe26eff0227fdb565c1271140f17c49b1039
AndySouth <southandy@gmail.com>
2016-02-16 22:42:31 +0000

    package doc

---
+++ R/rnaturalearthhires-package.r
@@ -0,0 +1,19 @@
+#' rnaturalearthhires : high resolution world vector maps from Natural Earth
+#'
+#' Facilitates world mapping by making \href{http://www.naturalearthdata.com/}{Natural Earth} map data more easily available to R users.
+#'
+#' @name rnaturalearthhires
+#' @docType package
+#' @seealso ne_countries
+#' @import sp
+NULL
+
+# Hide variables from R CMD check
+if (getRversion() >= "2.15.1") {
+  utils::globalVariables(c("countries10",
+                           "map_units10",
+                           "sovereignty10",
+                           "coastline10",
+                           "states10"
+                           ))
+}
