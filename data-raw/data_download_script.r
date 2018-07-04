3506495eebe466289ed45fe3f35d9549486396d3
AndySouth <southandy@gmail.com>
2018-07-04 17:57:05 +0100

    deleted old commented code

--- data-raw/data_download_script.r
+++ data-raw/data_download_script.r
@@ -25,22 +25,6 @@ coastline10 <- ne_download(scale=10, type='coastline', category='physical')
 #to allow same operation on all data objects in the package
 data_object_names <- data(package = "rnaturalearthhires")[["results"]][,"Item"]
 
-# 12/2/17 below replaced with rgdal::readOGR(iconv=TRUE) in ne_download()
-#dangerous code to remove all non-ascii characters
-#todo I might want to do something more intelligent than this
-#e.g. currently Curaçao is converted to Curaao
-# for (i in 1:length(data_object_names))
-# {
-#   data_name <- data_object_names[i]
-#   data_object <- eval(parse(text = data_name))
-#
-#   data_object@data <- data.frame( lapply(data_object@data, function(x) iconv(x, "latin1", "ASCII", sub="")))
-#
-#   #but now I want to reallocate back to the named object
-#   #beware this is dangerous
-#   eval(parse(text=paste(data_name,"<- data_object")))
-# }
-
 
 #### saving data files to correct folder in the package
 #this relies on working directory being set to root of the package
