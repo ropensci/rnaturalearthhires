16c0ac5a1c700fdeeea2a5fa6532294f245d6ea5
Michael Schramm <michael.schramm@agnet.tamu.edu>
2023-03-04 10:16:11 -0500

    temporary manual list

--- data-raw/data_download_script.r
+++ data-raw/data_download_script.r
@@ -24,7 +24,7 @@ coastline10 <- ne_download(scale=10, type='coastline', category='physical')
 
 #to allow same operation on all data objects in the package
 # data_object_names <- data(package = "rnaturalearthhires")[["results"]][,"Item"]
-data_object_names <- c("coastline10", "countries10", "mapunits10", "sovereignty10", "states10")
+#data_object_names <- c("coastline10", "countries10", "mapunits10", "sovereignty10", "states10")
 
 #### saving data files to correct folder in the package
 #this relies on working directory being set to root of the package
@@ -32,10 +32,16 @@ data_object_names <- c("coastline10", "countries10", "mapunits10", "sovereignty1
 # new data would have to be added outside of this
 
 #the lines below could be replaced by this dangerous eval(parse(text=)) loop
-for (i in 1:length(data_object_names))
-{
-  data_name <- data_object_names[i]
-  #eval(parse(text=paste0("save(",data_name,", file='data/",data_name,".rda'")))
-  #this sorts compression
-  eval(parse(text=paste0("usethis::use_data(",data_name,", compress='xz', overwrite=TRUE)")))
-}
+# for (i in 1:length(data_object_names))
+# {
+#   data_name <- data_object_names[i]
+#   #eval(parse(text=paste0("save(",data_name,", file='data/",data_name,".rda'")))
+#   #this sorts compression
+#   eval(parse(text=paste0("usethis::use_data(",data_name,", compress='xz', overwrite=TRUE)")))
+# }
+
+usethis::use_data(countries10, compress='xz', overwrite=TRUE)
+usethis::use_data(map_units10, compress='xz', overwrite=TRUE)
+usethis::use_data(sovereignty10, compress='xz', overwrite=TRUE)
+usethis::use_data(states10, compress='xz', overwrite=TRUE)
+usethis::use_data(coastline10, compress='xz', overwrite=TRUE)
